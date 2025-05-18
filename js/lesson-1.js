3931dd0d90b6dd83f4de45e97abb122b233b18be
Veronika Space <veronspaceless@gmail.com>
2025-05-18 20:14:41 +0300

    test

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -3,8 +3,8 @@
 // Запропонуйте користувачу ввести число за допомогою prompt().
 // Збережіть введене значення у змінну number.
 // Перевірте, чи дорівнює змінна number числу 10:
-//    Якщо так – виведіть повідомлення 'Вірно' у alert().
-//    Якщо ні – виведіть 'Невірно'. 
+// Якщо так  виведіть повідомлення 'Вірно' у alert().
+// Якщо ні  виведіть 'Невірно'. 
 
 // Підказка: prompt завжди повертає рядок (String), тому перед перевіркою  
 // перетворіть значення на число за допомогою Number().
@@ -16,9 +16,6 @@
 //     alert('Невірно');
 // }
 // або зробити те саме через тернарний оператор
-
-
-
 
 // Завдання 2:
 
@@ -80,6 +77,27 @@
 
 // console.log(checkSeason(userAnswer));
 
+
+
+// Завдання 4:
+
+// Отримуйте від користувача число (кількість хвилин) через prompt
+// і виводьте у консоль рядок у форматі годин та хвилин.
+// Приклад: користувач вводить в prompt '70' -> в консолі відобразиться '01:10'.
+// Корисне посилання для відображення годин та хвилин у потрібному форматі ('01' замість '1'): 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart#examples
+
+// const totalMinutes = Number(prompt('Введіть кількість хвилин'));
+
+// console.log(formatTime(totalMinutes));
+
+// function formatTime(totalMinutes) {
+//     const hours = Math.floor(totalMinutes / 60);
+//     const modifiedHours = String(hours).padStart(2, 0);
+//     const minutes = totalMinutes % 60;
+//     const modifiedMinutes = String(minutes).padStart(2, 0);
+//     return `${modifiedHours}:${modifiedMinutes}`;
+// }
 
 
 
